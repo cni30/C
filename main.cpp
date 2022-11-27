8c50358d8ec2dd5c135062b4855998c73b77d81c
cni30 <1362769051@qq.com>
2022-11-27 12:03:14 +0800

    堆优化的prim

--- main.cpp
+++ main.cpp
@@ -98,12 +98,7 @@ void prim(dequeGraph *graph, long long dis[], int path[], int n, int s)
             }
         }
         if (path[index] != 0)
-            printf("\n|%d|-->|%d|\tȨֵΪ:%d4"
-                   "0"
-                   "6"
-                   "3"
-                   "4"
-                   "6\n", path[index], index,dis[index]);
+            printf("\n|%d|-->|%d\tȨֵΪ: %d\n", path[index], index,dis[index]);
     }
 }
 
